5ee8d52a8ce01b1755f641044a9249d0b639784e
John Harris <john@s8r.uk>
2016-02-11 21:36:05 +0000

    Update a1upuall.sh

--- a1upuall.sh
+++ a1upuall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+case "$1" in
+"") echo "Usage: ${0##*/} <update register from Idox Elections>"; exit 1;;
+esac
 echo processing all updates...
-../a1upu.sh '*.ASC' all
-sort --field-separator=',' -k1,5 -k40 importedfullregister.csv importedupdateall.csv |
-sort -u --field-separator=',' -k1,5 |
-grep -v ',1 Delete 00' >importedcurrentregister.csv
+../a1upu.sh "$1" all
+sort -t$'\t' -k1,5 -k40 importedfullregister.csv importedupdateall.csv |
+sort -u -t$'\t' -k1,5 |
+grep -v '1 Delete 00' >importedcurrentregister.csv
+../reconciliation.sh
