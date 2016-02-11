b20830df1da3a987f2883ceef7ec466c9d8265eb
John Harris <john@s8r.uk>
2016-02-11 21:36:38 +0000

    Update a4full14.sh

--- a4full14.sh
+++ a4full14.sh
@@ -10,12 +10,19 @@ grep Forename |
 #type 1 if there's a surname+forename else 0.
 wc -l >type
 
-# drop Microsoft linefeed
-sed $'s/\r$//' < "$1" |
-# drop the header line(s)
-grep -v 'Elector Number Suffix' |
-awk -v p="pds.csv" -f ../b4full14.awk >importedfullregister.csv
+# take out the headers
+grep -v 'Franchise Flag' "$1" |
+grep -v 'Date Published' |
+grep -v 'Elector Number' |
+grep -v 'PostCode' |
+sed 's/\xa0/ /g' >awk1.tmp
+php ../maketab.php <awk1.tmp >awk2.tmp
+awk -v p="pds.csv" -f ../titlecase2.awk -f ../b4full14.awk awk2.tmp |
+sed 's/\.//g' >importedfullregister.csv
 wc -l importedfullregister.csv
 wc -l pds.csv
+grep BLANK importedfullregister.csv
+../constituencies.sh importedfullregister.csv
+echo "wc -l full.csv"
+rm awk?.tmp
 }
-echo ${0##*/} "$1 ended"
