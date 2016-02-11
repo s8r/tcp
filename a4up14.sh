e7696222b85af108ebc430016fa9a6ec4ee00861
John Harris <john@s8r.uk>
2016-02-11 21:37:17 +0000

    Update a4up14.sh

--- a4up14.sh
+++ a4up14.sh
@@ -2,11 +2,12 @@
 case "$2" in
 "") echo "Usage: ${0##*/} <update register from Xpress Software> <MM>"; exit 1;;
 esac
-time sed 's/\"\,\"/\jwhrg/g' < "$1" | 
-grep -v 'ElectorDOB' | 
-sed 's/^\"//' | 
-sed 's/\"$//' | 
-sed 's/\,//g' | 
-sed 's/jwhrg/\,/g' >awk2.tmp
-time awk -v p="pds.csv" -f ../b4up14.awk awk2.tmp >importedupdate$2.csv
-#srm -sr awk2.tmp
+time {
+grep -v 'ElectorDOB' "$1" | 
+sed 's/\xa0/ /g' |
+php ../maketab.php |
+awk -v p="pds.csv" -f ../b4up14.awk |
+sed 's/\.//g' >importedupdate$2.csv
+wc -l importedupdate$2.csv
+wc -l pds.csv
+}
